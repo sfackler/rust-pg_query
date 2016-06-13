3e20168f5c5466542a1ce57d58f67ed6fe5bc91d
Steven Fackler <sfackler@palantir.com>
2016-06-13 08:16:07 -0700

    WIP

--- pg_query/build/main.rs
+++ pg_query/build/main.rs
@@ -3,7 +3,7 @@ extern crate serde_json;
 
 use std::env;
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{BufReader, BufWriter, Write};
 use std::path::PathBuf;
 use std::collections::HashMap;
 
@@ -25,4 +25,101 @@ fn main() {
 
     let enum_defs: HashMap<String, HashMap<String, Enum>> =
         serde_json::from_reader(enum_defs).unwrap();
+
+    let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    let out = File::create(out_dir.join("types.rs")).unwrap();
+    let mut out = BufWriter::new(out);
+
+    make_node(&struct_defs, &mut out);
+}
+
+fn make_node(struct_defs: &HashMap<String, HashMap<String, Struct>>, out: &mut BufWriter<File>) {
+    write!(out,
+"pub enum Node {{
+").unwrap();
+    for (name, def) in &struct_defs["nodes/parsenodes"] {
+        write!(out,
+"    {} {{
+", name).unwrap();
+
+        for field in &def.fields {
+            let (name, c_type) = match (&field.name, &field.c_type) {
+                (&Some(ref name), &Some(ref c_type)) => (name, c_type),
+                _ => continue,
+            };
+
+            if name == "type" {
+                continue;
+            }
+
+            let rust_type = c_to_rust_type(c_type);
+
+            write!(out,
+"        {}: {},
+", name, rust_type).unwrap();
+        }
+
+        write!(out,
+"    }},
+").unwrap();
+    }
+
+    write!(out,
+"}}
+").unwrap();
+}
+
+fn c_to_rust_type(c_type: &str) -> &'static str {
+    match c_type {
+        "CmdType" => "CmdType",
+        "QuerySource" => "QuerySource",
+        "uint32" => "u32",
+        "bool" => "bool",
+        "Node*" => "Box<Node>",
+        "int" => "i32",
+        "List*" => "Vec<Node>",
+        "FromExpr*" => "FromExpr",
+        "OnConflictExpr*" => "OnConflictExpr",
+        "CreateStmt" => "Box<Node>",
+        "Oid" => "Oid",
+        "int32" => "i32",
+        "RangeVar*" => "RangeVar",
+        "char*" => "String",
+        "TypeName*" => "TypeName",
+        "GrantTargetType" => "GrantTargetType",
+        "GrantObjectType" => "GrantObjectType",
+        "WindowDef*" => "WindowDef",
+        "ObjectType" => "ObjectType",
+        "WithClause*" => "WithClause",
+        "RoleSpecType" => "RoleSpecType",
+        "XmlOptionType" => "XmlOptionType",
+        "DropBehavior" => "DropBehavior",
+        "OnCommitAction" => "OnCommitAction",
+        "Expr*" => "Expr",
+        "Alias*" => "Alias",
+        "DefElemAction" => "DefElemAction",
+        "int16" => "i16",
+        "char" => "u8",
+        "RoleStmtType" => "RoleStmtType",
+        "FunctionParameterMode" => "FunctionParameterMode",
+        "Index" => "Index",
+        "A_Expr_Kind" => "A_Expr_Kind",
+        "SortByDir" => "SortByDir",
+        "VariableSetStmt*" => "VariableSetStmt",
+        "IntoClause*" => "IntoClause",
+        "AlterTSConfigType" => "AlterTSConfigType",
+        "SortByNulls" => "SortByNulls",
+        "DiscardMode" => "DiscardMode",
+        "GrantStmt*" => "GrantStmt",
+        "LockClauseStrength" => "LockClauseStrength",
+        "RTEKind" => "RTEKind",
+        "SetOperation" => "SetOperation",
+        "LockWaitPolicy" => "LockWaitPolicy",
+        "VariableSetKind" => "VariableSetKind",
+        "Value" => "Value",
+        "OnConflictClause*" => "OnConflictClause",
+        "CollateClause*" => "CollateClause",
+        "SelectStmt*" => "Box<Node>",
+        t => panic!("unknown C type {}", t),
+    }
 }
